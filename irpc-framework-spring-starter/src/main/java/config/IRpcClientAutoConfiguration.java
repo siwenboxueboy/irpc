1e33dc3a75712e9c307d840114e19f31125500af
siwenboxue <siwenboxue@qq.com>
2023-07-22 22:32:42 +0800

    dubbo与spring整合的理解

--- irpc-framework-spring-starter/src/main/java/config/IRpcClientAutoConfiguration.java
+++ irpc-framework-spring-starter/src/main/java/config/IRpcClientAutoConfiguration.java
@@ -13,6 +13,13 @@ import org.springframework.context.ApplicationListener;
 
 import java.lang.reflect.Field;
 
+/**
+ * 该类是如何进入到spring容器中的？通过spring boot 提供的spi机制加载的<br>
+ * 为什么不直接加 @EnableAutoConfiguration 注解<br>
+ * 用户如果使用的话，使用方法是：该实现会作为jar包依赖于使用方的lib中<br>
+ * 而spring boot的包扫描路径是主类的子包路径，并不会包含该路径<br>
+ * 但是spring boot 有SPI机制，jar包中实现了spring boot 的SPI，spring boot就会把它加载进spring context中处理
+ * */
 @Slf4j
 public class IRpcClientAutoConfiguration implements BeanPostProcessor, ApplicationListener<ApplicationReadyEvent> {
 
@@ -20,12 +27,18 @@ public class IRpcClientAutoConfiguration implements BeanPostProcessor, Applicati
     private static Client client = null;
     private volatile boolean needInitClient = false;
     private volatile boolean hasInitClientConfig = false;
-
+    /**
+     * 阶段：在由bean Definition 初始化对象之后<br>
+     * 职责：<br>
+     * 1. 初始化rpc 客户端<br>
+     * 2. 接入spring：将bean 实例化后的属性进行过滤，如果属性加了IRpcReference注解，则给其进行属性依赖注入。注入过程是利用动态代理，生成代理对象，详见RpcReference<br>
+     * */
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         Field[] fields = bean.getClass().getDeclaredFields();
         for (Field field : fields) {
             if (field.isAnnotationPresent(IRpcReference.class)) {
+                // 客户端只会初始化一次
                 if (!hasInitClientConfig) {
                     client = new Client();
                     try {
--- irpc-framework-spring-starter/src/main/java/config/IRpcServerAutoConfiguration.java
+++ irpc-framework-spring-starter/src/main/java/config/IRpcServerAutoConfiguration.java
@@ -17,9 +17,17 @@ import java.util.Map;
 public class IRpcServerAutoConfiguration implements InitializingBean, ApplicationContextAware {
     private ApplicationContext applicationContext;
 
+    /**
+     * 阶段：对象初始化阶段，属性填充之后<br>
+     * 职责：<br>
+     * 1. 初始化rpc server<br>
+     * 2. 接入spring：从spring factory拿到服务接口组装Provider信息，放入本地缓存，后续暴露给注册中心<br>
+     * */
     @Override
     public void afterPropertiesSet() throws Exception {
         Server server = null;
+        // IRpcService作用：1. 用于标记rpc 服务提供者接口，2. 同时也可以注入spring
+        // 拿到标记了IRpcService注解的 spring bean
         Map<String, Object> beanMap = applicationContext.getBeansWithAnnotation(IRpcService.class);
         if (beanMap.size() == 0) {
             //说明当前应用内部不需要对外暴露服务
@@ -27,8 +35,11 @@ public class IRpcServerAutoConfiguration implements InitializingBean, Applicatio
         }
         printBanner();
         long begin = System.currentTimeMillis();
+        // 初始化网络服务
         server = new Server();
+        // 1. 配置加载 2. SPI加载
         server.initServerConfig();
+        // 初始化发布-订阅模型
         IRpcListenerLoader iRpcListenerLoader = new IRpcListenerLoader();
         iRpcListenerLoader.init();
         for (String beanName : beanMap.keySet()) {
@@ -37,11 +48,13 @@ public class IRpcServerAutoConfiguration implements InitializingBean, Applicatio
             ServiceWrapper dataServiceServiceWrapper = new ServiceWrapper(bean, iRpcService.group());
             dataServiceServiceWrapper.setServiceToken(iRpcService.serviceToken());
             dataServiceServiceWrapper.setLimit(iRpcService.limit());
+            // 1. SPI加载注册中心配置，实例化注册中心 2. 服务接口组装Provider信息，放入本地缓存，后续暴露给注册中心
             server.exportService(dataServiceServiceWrapper);
             log.info(">>>>>>>>>>>>>>> [irpc] {} export success! >>>>>>>>>>>>>>> ", beanName);
         }
         long end = System.currentTimeMillis();
         ApplicationShutdownHook.registryShutdownHook();
+        // 1. 启动netty相关基础组件 2. 向注册中心注册相关服务接口 3. 启动接收rpc请求线程
         server.startApplication();
         log.info(" ================== [{}] started success in {}s ================== ", server.getServerConfig().getApplicationName(), ((double) end - (double) begin) / 1000);
     }
